b8e2183ef1cf462c8431785c52580124d36f62fc
tiboun <bounkong.khamphousone@ebiznext.com>
2017-03-14 22:18:48 +0100

    correction de l'url des appels ajax

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server: string = 'http://localhost:3000/';
+    public Server: string = '/';
     public ApiUrl: string = 'api';
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
